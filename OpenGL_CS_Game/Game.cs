89825dac3f4d9937005f22ccf00bb8d7f00f04db
AlexGreen <ZAA93@yandex.ru>
2015-09-26 16:00:55 +0300

    Fixes, TextureLoad -> Texture.cs.

--- OpenGL_CS_Game/Game.cs
+++ OpenGL_CS_Game/Game.cs
@@ -100,25 +100,25 @@ namespace OpenGL_CS_Game
                     // Исли не указаны текстурные фильтры, тогда
                     if (StrMagFilter == String.Empty && StrMinFilter == String.Empty)
                     {
-                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(loadImage(
+                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(Texture.LoadImage(
                             Path.Combine(TexturesPath, xmlNode.SelectSingleNode("File").InnerText))));
                     }
                     else if (StrMagFilter == String.Empty)
                     {
-                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(loadImage(
+                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(Texture.LoadImage(
                             Path.Combine(TexturesPath, xmlNode.SelectSingleNode("File").InnerText),
                             TextureMagFilter.Linear,
                             (TextureMinFilter)Enum.Parse(typeof(TextureMinFilter), StrMinFilter, true))));
                     }
                     else if (StrMinFilter == String.Empty)
                     {
-                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(loadImage(
+                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(Texture.LoadImage(
                             Path.Combine(TexturesPath, xmlNode.SelectSingleNode("File").InnerText),
                             (TextureMagFilter)Enum.Parse(typeof(TextureMagFilter), StrMagFilter, true))));
                     }
                     else
                     {
-                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(loadImage(
+                        textures.Add(xmlNode.SelectSingleNode("Name").InnerText, new Texture(Texture.LoadImage(
                             Path.Combine(TexturesPath, xmlNode.SelectSingleNode("File").InnerText),
                             (TextureMagFilter)Enum.Parse(typeof(TextureMagFilter), StrMagFilter, true),
                             (TextureMinFilter)Enum.Parse(typeof(TextureMinFilter), StrMinFilter, true))));
@@ -147,14 +147,18 @@ namespace OpenGL_CS_Game
                     {
                         Bitmap[] CubeMapsBMP = new Bitmap[TMPXmlNodeList.Count];
 
+                        int SideSize = -1;
+                        if (xmlNode.SelectNodes("SideSize").Count > 0 && xmlNode.SelectSingleNode("SideSize").InnerText.Trim() != String.Empty)
+                            SideSize = int.Parse(xmlNode.SelectSingleNode("SideSize").InnerText);
+
                         for (int i = 0; i < TMPXmlNodeList.Count; i++)
                             CubeMapsBMP[i] = new Bitmap(Path.Combine(CubeMapDir, TMPXmlNodeList[i].InnerText));
 
-                        int SideSize = 0;
-                        if (xmlNode.SelectNodes("SideSize").Count > 0 && xmlNode.SelectSingleNode("SideSize").InnerText != String.Empty)
-                            SideSize = int.Parse(xmlNode.SelectSingleNode("SideSize").InnerText);
+                        if (SideSize > 0)
+                            for (int i = 0; i < TMPXmlNodeList.Count; i++)
+                                CubeMapsBMP[i] = new Bitmap(CubeMapsBMP[i], SideSize, SideSize);
 
-                        textures.Add(Name, new Texture(loadCubeMap(CubeMapsBMP, SideSize), TextureTarget.TextureCubeMap));
+                        textures.Add(Name, new Texture(Texture.LoadCubeMap(CubeMapsBMP), TextureTarget.TextureCubeMap));
                         CubeMapsBMP = null;
                     }
                     else
@@ -295,7 +299,7 @@ namespace OpenGL_CS_Game
 
             // Создаем примитивы
             Cube cube = new Cube(zFar);
-            cube.Material = materials["SkyCubemap_Test"];
+            cube.Material = materials["SkyCubemap_Storforsen"];
             objects.Add(cube);
 
             //Plain plain = new Plain();
@@ -536,116 +540,11 @@ namespace OpenGL_CS_Game
 
             // Обновляем позиции объектов
             time += (float)e.Time;
-            objects[0].Rotation += new Vector3(0, -0.001f * time, 0);
+            objects[0].Rotation += new Vector3(0, -0.01f, 0);
 
             Angle += rotSpeed * (float)e.Time;
             if (Angle > MathHelper.TwoPi)
                 Angle -= MathHelper.TwoPi;
-        }
-
-        int loadImage(string filename, TextureMagFilter MagFilter = TextureMagFilter.Linear, TextureMinFilter MinFilter = TextureMinFilter.LinearMipmapLinear)
-        {
-            try
-            {
-                Bitmap file = new Bitmap(filename);
-                return loadImage(file, MagFilter, MinFilter);
-            }
-            catch (FileNotFoundException e)
-            {
-                MessageBox.Show("Error: Cannot load image.\n" + e.Message);
-                return -1;
-            }
-        }
-
-        int loadImage(Bitmap image, TextureMagFilter MagFilter = TextureMagFilter.Linear, TextureMinFilter MinFilter = TextureMinFilter.LinearMipmapLinear)
-        {
-            // Отображаем текстуру по Y
-            image.RotateFlip(RotateFlipType.RotateNoneFlipY);
-
-            int texID = GL.GenTexture();
-
-            GL.BindTexture(TextureTarget.Texture2D, texID);
-            BitmapData data = image.LockBits(new System.Drawing.Rectangle(0, 0, image.Width, image.Height),
-                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-
-            //// Allocate storage
-            //GL.TexStorage2D(TextureTarget2d.Texture2D, 1, SizedInternalFormat.Rgba8, image.Width, image.Height);
-            //// Copy data into storage
-            //GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, image.Width, image.Height,
-            //    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-
-            image.UnlockBits(data);
-
-            //Filter params
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)MagFilter);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)MinFilter);
-
-            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-
-            return texID;
-        }
-
-        int loadCubeMap(Bitmap[] CubeMapTextures)
-        {
-            return loadCubeMap(CubeMapTextures, -1);
-        }
-
-        int loadCubeMap(Bitmap[] CubeMapTextures, int TextureSideSize)
-        {
-            if (CubeMapTextures.Length == 6)
-            {
-                // Изменяем, отображаем и вращаем текстуры
-                for (int i = 0; i < CubeMapTextures.Length; i++)
-                {
-                    if (TextureSideSize > 0)
-                        CubeMapTextures[i] = new Bitmap(CubeMapTextures[i], TextureSideSize, TextureSideSize);
-                    if (i < 2 || i > 3)
-                        CubeMapTextures[i].RotateFlip(RotateFlipType.RotateNoneFlipX);
-                    if (i == 2 || i == 3)
-                        CubeMapTextures[i].RotateFlip(RotateFlipType.Rotate270FlipY);
-                    if (i == 2)
-                        CubeMapTextures[i].RotateFlip(RotateFlipType.Rotate180FlipNone);
-                }
-
-                int texID = GL.GenTexture();
-                GL.ActiveTexture(TextureUnit.Texture0);
-                GL.BindTexture(TextureTarget.TextureCubeMap, texID);
-
-                TextureTarget[] targets = new TextureTarget[]
-                {
-                    TextureTarget.TextureCubeMapPositiveX, TextureTarget.TextureCubeMapNegativeX,
-                    TextureTarget.TextureCubeMapNegativeY, TextureTarget.TextureCubeMapPositiveY,
-                    TextureTarget.TextureCubeMapPositiveZ, TextureTarget.TextureCubeMapNegativeZ
-                };
-
-                // Allocate storage
-                GL.TexStorage2D(TextureTarget2d.TextureCubeMap, 1, SizedInternalFormat.Rgba8,
-                    CubeMapTextures[0].Width, CubeMapTextures[0].Height);
-
-                BitmapData data;
-                // Загружаем все текстуры граней
-                for (int i = 0; i < CubeMapTextures.Length; i++)
-                {
-                    data = CubeMapTextures[i].LockBits(new System.Drawing.Rectangle(0, 0, CubeMapTextures[i].Width,
-                        CubeMapTextures[i].Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                    GL.TexSubImage2D(targets[5 - i], 0, 0, 0, data.Width, data.Height, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-                    CubeMapTextures[i].UnlockBits(data);
-                }
-
-                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
-                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
-                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapR, (int)TextureWrapMode.ClampToEdge);
-
-                //GL.GenerateMipmap(GenerateMipmapTarget.TextureCubeMap);
-                return texID;
-            }
-            else
-                return -1;
         }
     }
 }
--- OpenGL_CS_Game/Texture.cs
+++ OpenGL_CS_Game/Texture.cs
@@ -1,4 +1,9 @@
-﻿using OpenTK;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace OpenGL_CS_Game
@@ -33,6 +38,104 @@ namespace OpenGL_CS_Game
         {
             get { return textureTarget; }
             set { textureTarget = value; }
+        }
+
+        public static int LoadImage(string filename, TextureMagFilter MagFilter = TextureMagFilter.Linear, TextureMinFilter MinFilter = TextureMinFilter.LinearMipmapLinear)
+        {
+            try
+            {
+                Bitmap file = new Bitmap(filename);
+                return LoadImage(file, MagFilter, MinFilter);
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageBox.Show("Error: Cannot load image.\n" + e.Message);
+                return -1;
+            }
+        }
+
+        public static int LoadImage(Bitmap image, TextureMagFilter MagFilter = TextureMagFilter.Linear, TextureMinFilter MinFilter = TextureMinFilter.LinearMipmapLinear)
+        {
+            // Отображаем текстуру по Y
+            image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+            int texID = GL.GenTexture();
+
+            GL.BindTexture(TextureTarget.Texture2D, texID);
+            BitmapData data = image.LockBits(new System.Drawing.Rectangle(0, 0, image.Width, image.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            //// Allocate storage
+            //GL.TexStorage2D(TextureTarget2d.Texture2D, 1, SizedInternalFormat.Rgba8, image.Width, image.Height);
+            //// Copy data into storage
+            //GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, image.Width, image.Height,
+            //    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            image.UnlockBits(data);
+
+            //Filter params
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)MagFilter);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)MinFilter);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+
+            return texID;
+        }
+
+        public static int LoadCubeMap(Bitmap[] CubeMapTextures)
+        {
+            if (CubeMapTextures.Length == 6)
+            {
+                // Изменяем, отображаем и вращаем текстуры
+                for (int i = 0; i < CubeMapTextures.Length; i++)
+                {
+                    if (i < 2 || i > 3)
+                        CubeMapTextures[i].RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    if (i == 2 || i == 3)
+                        CubeMapTextures[i].RotateFlip(RotateFlipType.Rotate270FlipY);
+                    if (i == 2)
+                        CubeMapTextures[i].RotateFlip(RotateFlipType.Rotate180FlipNone);
+                }
+
+                int texID = GL.GenTexture();
+                GL.ActiveTexture(TextureUnit.Texture0);
+                GL.BindTexture(TextureTarget.TextureCubeMap, texID);
+
+                TextureTarget[] targets = new TextureTarget[]
+                {
+                    TextureTarget.TextureCubeMapPositiveX, TextureTarget.TextureCubeMapNegativeX,
+                    TextureTarget.TextureCubeMapNegativeY, TextureTarget.TextureCubeMapPositiveY,
+                    TextureTarget.TextureCubeMapPositiveZ, TextureTarget.TextureCubeMapNegativeZ
+                };
+
+                // Allocate storage
+                GL.TexStorage2D(TextureTarget2d.TextureCubeMap, 1, SizedInternalFormat.Rgba8,
+                    CubeMapTextures[0].Width, CubeMapTextures[0].Height);
+
+                BitmapData data;
+                // Загружаем все текстуры граней
+                for (int i = 0; i < CubeMapTextures.Length; i++)
+                {
+                    data = CubeMapTextures[i].LockBits(new System.Drawing.Rectangle(0, 0, CubeMapTextures[i].Width,
+                        CubeMapTextures[i].Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                    GL.TexSubImage2D(targets[5 - i], 0, 0, 0, data.Width, data.Height, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+                    CubeMapTextures[i].UnlockBits(data);
+                }
+
+                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
+                GL.TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapR, (int)TextureWrapMode.ClampToEdge);
+
+                //GL.GenerateMipmap(GenerateMipmapTarget.TextureCubeMap);
+                return texID;
+            }
+            else
+                return -1;
         }
     }
 }
